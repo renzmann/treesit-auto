af691cb09008730c9708cab6b37b28ee1738ca8c
renzmann <32076780+renzmann@users.noreply.github.com>
2023-01-22 13:36:17 -0500

    Initial commit of treesit-fallback.el

---
+++ treesit-fallback.el
@@ -0,0 +1,112 @@
+;;; treesit-fallback.el --- Fallback from `-ts-mode' to another major mode -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022 Robert Enzmann
+
+;; Author: Robb Enzmann <robbenzmann@gmail.com>
+;; Keywords: treesitter mode fallback
+;; URL: https://github.com/renzmann/treesit-fallback
+;; Version: 0.1.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; If a tree-sitter grammer is available and installed, use it instead of the
+;; corresponding default mode.  Conversely, when a tree-sitter grammar is not
+;; available and a fallback major mode is available/specified, use it instead.
+
+;;; Code:
+(require 'treesit)
+
+(setq treesit-fallback--language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (c "https://github.com/tree-sitter/tree-sitter-c")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
+        (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (csharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (go "https://github.com/tree-sitter/tree-sitter-go")
+        (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (lua "https://github.com/Azganoth/tree-sitter-lua")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (r "https://github.com/r-lib/tree-sitter-r")
+        (rust "https://github.com/tree-sitter/tree-sitter-rust")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+        (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+(dolist (elt treesit-fallback--language-source-alist)
+        (add-to-list 'treesit-language-source-alist elt t))
+
+(defcustom treesit-fallback-modes
+  (mapcar
+   (lambda (elt)
+     (cons (purecopy (car elt)) (cdr elt)))
+   `((toml-ts-mode . conf-toml-mode)
+     ;; TODO: I do not know if the future treesitter mode for HTML will be called html-ts-mode or mhtml-mode
+     (html-ts-mode . mhtml-mode)
+     ;; See deprecation note in their README: https://github.com/emacs-typescript/typescript.el#a-short-note-on-development-halt
+     (typescript-ts-mode . nil)
+     (tsx-ts-mode . nil)))
+  "Alist mapping treesitter modes to their respective fallback modes.
+If the CDR of the association is nil, then no fallback will be
+attempted when encountering a tree-sitter mode that is missing an
+installation of its respecitve grammar.  If the CDR is non-nil,
+then a fallback attempt is made to the specified mode.
+
+If a treesitter mode is omitted from the keys of this alist
+entirely, then a fallback is attempted by using the same name
+prefix (e.g. `python-ts-mode' will attempt a fallback to
+`python-mode').
+
+In any case, if the fallback mode does not
+exist (e.g. go-mod-mode is not installed but that would be the
+automatic fallback for go-mod-ts-mode), then no fallback is
+attempted."
+  :type '(alist (symbol) (function))
+  :group 'treesit)
+
+(defun treesit-fallback--remap-language-source (language-source)
+  "Add a major mode's corresponding ts mode to major-mode-remap-alist, if it's ready."
+  (let* ((name (car language-source))
+         (name-ts-mode (intern (concat (symbol-name name) "-ts-mode")))
+         (fallback-assoc (assq name-ts-mode treesit-fallback-modes))
+         (fallback-name (cdr fallback-assoc))
+         (name-mode (or fallback-name
+                        (intern (concat (symbol-name name) "-mode"))))
+         (name-mode-bound-p (fboundp name-mode))
+         (skip-remap-p (and fallback-assoc
+                            (not (cdr fallback-assoc)))))
+    (and (fboundp name-ts-mode)
+         (if (treesit-ready-p name t)
+             (add-to-list 'major-mode-remap-alist `(,name-mode . ,name-ts-mode))
+           (when name-mode-bound-p
+             (add-to-list 'major-mode-remap-alist `(,name-ts-mode . ,name-mode)))))))
+
+(defun treesit-fallback-apply-remap ()
+  "Examine installed grammars+modes and adjust `major-mode-remap-alist' accordingly."
+  (mapcar 'treesit-fallback--remap-language-source treesit-language-source-alist))
+
+(advice-add 'treesit-install-language-grammar :after (lambda (&rest r) (treesit-fallback-apply-remap)))
+(treesit-fallback-apply-remap)
+
+(provide 'treesit-fallback)
+;;; treesit-fallback.el ends here
