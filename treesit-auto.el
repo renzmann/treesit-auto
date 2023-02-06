9d01a05d5d65790283360b39679ec01bdbdc43cd
Robb Enzmann <robbenzmann@gmail.com>
2023-02-06 16:48:55 -0500

    Bump version: 0.4.0 → 0.4.1

--- treesit-auto.el
+++ treesit-auto.el
@@ -5,7 +5,7 @@
 ;; Author: Robb Enzmann <robbenzmann@gmail.com>
 ;; Keywords: treesitter auto automatic major mode fallback convenience
 ;; URL: https://github.com/renzmann/treesit-auto.git
-;; Version: 0.4.0
+;; Version: 0.4.1
 ;; Package-Requires: ((emacs "29.0"))
 
 ;; This file is not part of GNU Emacs.
