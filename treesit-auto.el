91bb99dc6fd16ff24d4aad1af72d811b08c5c6e3
Robb Enzmann <robbenzmann@gmail.com>
2023-02-07 14:17:21 -0500

    Fix bug where selecting "n" would install grammars anyway

--- treesit-auto.el
+++ treesit-auto.el
@@ -264,12 +264,11 @@ Individual grammars can be opted out of by adding them to
                               (mapconcat 'symbol-name to-install "\n"))))
     ;; TODO QOL - it would be nice if this messaged what was installed or at
     ;; least mentioned that nothing was installed if skipped.
-    (unless (eq treesit-auto-install t) ; Quiet mode is off
-      ;; TODO de-couple this?  Allow for prefix to prompt?
-      (with-output-to-temp-buffer "*Treesit-auto install candidates*"
-        (princ prompt))
-      (y-or-n-p "Install missing grammars? "))
-    (mapcar 'treesit-install-language-grammar to-install)))
+    (with-output-to-temp-buffer "*Treesit-auto install candidates*"
+      (princ prompt))
+    (when (or (eq treesit-auto-install t) ; Quiet mode is off
+              (y-or-n-p "Install missing grammars? "))
+      (mapcar 'treesit-install-language-grammar to-install))))
 
 ;;;###autoload
 (defun treesit-auto-apply-remap ()
